83e77c47904db17084eaea28da629f8f5e1dbc7e
fleuarison <137941408+fleuarison@users.noreply.github.com>
2023-08-17 18:35:35 +0300

    Added weather forecast API

--- src/app.js
+++ src/app.js
@@ -23,6 +23,11 @@ function formatDate(date) {
   let dateToday = document.querySelector("#date");
   dateToday.innerHTML = formatDate("now");
   
+  function getForecast(coordinates){
+    let apiKey = "6782253072f7d90462731a624097fc54";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units="metric"`
+    axios.get(apiUrl).then(displayForecast);
+  }
   function weatherCity(response) {
     celsiusTemperature = response.data.main.temp;
     document.querySelector("#new-city").innerHTML = response.data.name;
@@ -38,6 +43,7 @@ function formatDate(date) {
   iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
+  getForecast(response.data.coord);
   
   }
 function displayFahreinheitTemperature(event){
@@ -77,7 +83,6 @@ let celsiusTemperature = null;
   searchForm.addEventListener("submit", handleSubmit);
   
   search("Antananarivo");
-  displayForecast();
   
   function showPosition(position) {
     let apiKey = "6782253072f7d90462731a624097fc54";
@@ -93,7 +98,7 @@ let celsiusTemperature = null;
   let locationButton = document.querySelector("#location");
   locationButton.addEventListener("click", getCurrentPosition);
 
-  function displayForecast() {
+  function displayForecast(response) {
     let forecastElement = document.querySelector("#forecast");
     let forecastHTML = `<div class="row">`;
     let days = ["Tue","Wen","Thu","Fri","Sat"];
